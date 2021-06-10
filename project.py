def99836ba3edec4a84ecefd6bb5b60db6ec78d5
yunii23 <67429918+yunii23@users.noreply.github.com>
2021-06-10 18:52:22 +0900

    Add files via upload

--- project.py
+++ project.py
@@ -51,6 +51,81 @@ accuracy_scores = {}
 
 
 
+def predictor(predictor, params): 
+    
+    global accuracy_scores
+    if predictor == 'lr':
+        print('Training Logistic Regression on Training Set')
+        from sklearn.linear_model import LogisticRegression
+        classifier = LogisticRegression(**params)
 
+    elif predictor == 'svm':
+        print('Training Support Vector Machine on Training Set')
+        from sklearn.svm import SVC
+        classifier = SVC(**params)
+
+    elif predictor == 'knn':
+        print('Training K-Nearest Neighbours on Training Set')
+        from sklearn.neighbors import KNeighborsClassifier
+        classifier = KNeighborsClassifier(**params)
+
+    elif predictor == 'dt':
+        print('Training Decision Tree Classifier on Training Set')
+        from sklearn.tree import DecisionTreeClassifier
+        classifier = DecisionTreeClassifier(**params)
+
+    elif predictor == 'rfc':
+        print('Training Random Forest Classifier on Training Set')
+        from sklearn.ensemble import RandomForestClassifier
+        classifier = RandomForestClassifier(**params)
+        
+
+    classifier.fit(X_train, y_train)
+
+    print('''Predicting Single Cell Result''')
+    single_predict = classifier.predict(sc.transform([[
+        7.4, 0.7, 0.0, 1.9, 0.076, 11.0, 34.0, 0.9978, 3.51, 0.56, 9.4
+    ]]))
+    if single_predict > 0 :
+        print('High Quality Wine')
+    else:
+        print('Low Quality Wine')
+    print('''Prediciting Test Set Result''')
+    y_pred = classifier.predict(X_test)
+    
+    result = np.concatenate((y_pred.reshape(len(y_pred), 1),
+                              y_test.reshape(len(y_test), 1)),1)
+    #print(result, '\n')
+    print('''Making Confusion Matrix''')
+    from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+    y_pred = classifier.predict(X_test)
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm, '\n')
+    plot_confusion_matrix(classifier, X_test, y_test, cmap="pink")
+    print('True Positives :', cm[0][0])
+    print('False Positives :', cm[0][1])
+    print('False Negatives :', cm[1][0])
+    print('True Negatives :', cm[0][1], '\n')
+
+    print('''Classification Report''')
+    print(classification_report(y_test, y_pred,
+          target_names=['0', '1'], zero_division=1))
+
+    print('''Evaluating Model Performance''')
+    accuracy = accuracy_score(y_test, y_pred)
+    print(accuracy, '\n')
+
+    print('''Applying K-Fold Cross validation''')
+    from sklearn.model_selection import cross_val_score
+    accuracies = cross_val_score(
+        estimator=classifier, X=X_train, y=y_train, cv=10)
+    print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+    accuracy_scores[classifier] = accuracies.mean()*100
+    print("Standard Deviation: {:.2f} %".format(accuracies.std()*100), '\n')
     
     
+#predictor('lr', {'penalty': 'l2', 'solver': 'sag'})    
+
+#predictor('svm', {'C': 15, 'gamma': 0.1, 'kernel': 'rbf', 'random_state': 0})
+
+predictor('knn', {'algorithm': 'auto', 'n_jobs': 1, 'n_neighbors': 17, 'weights': 'distance'})
