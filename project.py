0a4f693c4496120b98db7b81d5c2562471965465
yunii23 <67429918+yunii23@users.noreply.github.com>
2021-06-10 18:51:38 +0900

    Add files via upload

--- project.py
+++ project.py
@@ -33,12 +33,21 @@ dataset['quality'].value_counts()
 X = dataset.iloc[:, 0:-1].values
 y = dataset.iloc[:, -1].values
 
-print(X)
-print(y)
-print(X.shape)
-print(y.shape)
+#print(X)
+#print(y)
+#print(X.shape)
+#print(y.shape)
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=1)
 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+accuracy_scores = {}
 
 
 
